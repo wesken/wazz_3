bcb2cf16194604f4d8d69bb5729a97080f95276d
wesken <wesken@zoho.com>
2016-09-02 23:17:22 -0400

    added bla bra

--- Wazz_2/ViewController.swift
+++ Wazz_2/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("1233")
+        print("bla bla")
+        
         
         // Do any additional setup after loading the view, typically from a nib.
     }
